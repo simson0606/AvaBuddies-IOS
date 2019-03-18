5d51f0f7220504f61d6538dfa2a584585000a712
Simon Heij <sheij1@avans.nl>
2019-03-18 11:55:58 +0100

    Added gitignore.

---
+++ AvaBuddies/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  AvaBuddies
+//
+//  Created by simon heij on 18/03/2019.
+//  Copyright © 2019 simon heij. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: ViewController {
+
+}
