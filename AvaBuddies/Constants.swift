7b7bb36a1aeafaa3fbd1a55a3709ed4cedd3be22
Simon Heij <sheij1@avans.nl>
2019-04-15 13:15:12 +0200

    set url to live environment

--- AvaBuddies/Constants.swift
+++ AvaBuddies/Constants.swift
@@ -21,7 +21,7 @@ struct Constants {
     }
     
     struct ServerConnection {
-        static let BaseURL = "https://dev.avabuddies.nl"
+        static let BaseURL = "https://avabuddies.nl"
         static let RegisterRoute = "/auth/signup"
         static let LoginRoute = "/auth/login"
         static let UserProfileRoute = "/user/profile"
