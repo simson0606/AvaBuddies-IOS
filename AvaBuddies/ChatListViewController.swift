600fae00fb01a35f1ac449baac02da7900ad1ac6
Simon Heij <sheij1@avans.nl>
2019-04-23 12:50:40 +0200

    addeded views

---
+++ AvaBuddies/ChatListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatListViewController.swift
+//  AvaBuddies
+//
+//  Created by simon heij on 23/04/2019.
+//  Copyright © 2019 simon heij. All rights reserved.
+//
+
+import UIKit
+
+class ChatListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+
+}
