d3dc8bc0d01457c5fdfc266a70b9b3320578dc8d
Bryan van Lierop <bryanvlierop96@gmail.com>
2019-05-11 14:26:02 +0200

    Private tags

--- AvaBuddies/Tag.swift
+++ AvaBuddies/Tag.swift
@@ -15,4 +15,12 @@ struct TagListResponse : Codable {
 struct Tag : Codable, Equatable {
     var _id: String
     var name: String
+    var isPrivate: Bool = false
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self._id = try container.decodeIfPresent(String.self, forKey: ._id) ?? ""
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        self.isPrivate = try container.decodeIfPresent(Bool.self, forKey: .isPrivate) ?? false
+    }
 }
